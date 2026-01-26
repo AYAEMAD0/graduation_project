1b2a682e0a1a690372502592f92c876714ed41f5
Aya Emad <ayaelbaroudy0@gmail.com>
2026-01-26 19:09:24 +0200

    feat: implement app styles, font weights, colors, and gradients using Google Fonts and ScreenUtil

---
+++ lib/core/theme/app_color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color primaryBlueColor = Color(0xff6A98DD);
+  static const Color primaryPurpleColor = Color(0xffBC83F8);
+  static const Color whiteColor = Color(0xffFFFFFF);
+  static const Color blackColor = Color(0xff000000);
+  static const Color grayColor = Color(0xff515151);
+  static const Color grayDarkColor = Color(0xff282828);
+  static const Color transparentColor = Colors.transparent;
+
+}
---
+++ lib/core/theme/app_style.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'app_color.dart';
+import 'font_weight_helper.dart';
+
+class AppStyle {
+  static final TextStyle font34BlackSemiBold = GoogleFonts.roboto(
+    fontSize: 34.sp,
+    color: AppColor.blackColor,
+    fontWeight: FontWeightHelper.semiBold,
+  );
+
+  static final TextStyle font27BlackLight = GoogleFonts.roboto(
+    fontSize: 27.sp,
+    color: AppColor.blackColor,
+    fontWeight: FontWeightHelper.light,
+  );
+
+  static final TextStyle font16WhiteBold = GoogleFonts.roboto(
+    fontSize: 16.sp,
+    color: AppColor.whiteColor,
+    fontWeight: FontWeightHelper.bold,
+  );
+
+  static final TextStyle font24BlackBold = GoogleFonts.roboto(
+    fontSize: 24.sp,
+    color: AppColor.blackColor,
+    fontWeight: FontWeightHelper.bold,
+  );
+
+  static final TextStyle font14GrayRegular = GoogleFonts.roboto(
+    fontSize: 14.sp,
+    color: AppColor.grayColor,
+    fontWeight: FontWeightHelper.regular,
+  );
+
+  static final TextStyle font14GrayMedium = GoogleFonts.roboto(
+    fontSize: 14.sp,
+    color: AppColor.grayColor,
+    fontWeight: FontWeightHelper.medium,
+  );
+
+  static final TextStyle font15GrayDarkRegular = GoogleFonts.roboto(
+    fontSize: 15.sp,
+    color: AppColor.grayDarkColor,
+    fontWeight: FontWeightHelper.regular,
+  );
+
+  static final TextStyle font16GrayRegular = GoogleFonts.roboto(
+    fontSize: 16.sp,
+    color: AppColor.grayColor,
+    fontWeight: FontWeightHelper.regular,
+  );
+
+  static final TextStyle font16BlackBold = GoogleFonts.roboto(
+    fontSize: 16.sp,
+    color: AppColor.blackColor,
+    fontWeight: FontWeightHelper.bold,
+  );
+
+  static final TextStyle font32BlackBold = GoogleFonts.roboto(
+    fontSize: 32.sp,
+    color: AppColor.blackColor,
+    fontWeight: FontWeightHelper.bold,
+  );
+
+  static final TextStyle font17BlackRegular = GoogleFonts.roboto(
+    fontSize: 17.sp,
+    color: AppColor.blackColor,
+    fontWeight: FontWeightHelper.regular,
+  );
+
+  static final TextStyle font18BlackRegular = GoogleFonts.roboto(
+    fontSize: 18.sp,
+    color: AppColor.blackColor,
+    fontWeight: FontWeightHelper.regular,
+  );
+
+  static final TextStyle font16BlackRegular = GoogleFonts.roboto(
+    fontSize: 16.sp,
+    color: AppColor.blackColor,
+    fontWeight: FontWeightHelper.regular,
+  );
+
+  static final TextStyle font18WhiteBold = GoogleFonts.roboto(
+    fontSize: 18.sp,
+    color: AppColor.whiteColor,
+    fontWeight: FontWeightHelper.bold,
+  );
+}
