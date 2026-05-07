2f19d1aa9763de35e22d120b69ee35a31830e284
Aya Emad <ayaelbaroudy0@gmail.com>
2026-05-07 16:36:22 +0300

    + test deploy after change vercel.json

--- lib/api/api_endpoint.dart
+++ lib/api/api_endpoint.dart
@@ -1,28 +1,53 @@
+// class ApiEndpoint {
+//   //todo static const String baseUrl = 'http://mockmate-001-site1.mtempurl.com';
+//   static const String baseUrl = '';
+//   static const String signupApi = '/api/users';
+//   static const String loginApi = "/api/users/login";
+//   static const String refreshTokenApi = '/api/users/refresh';
+//   static const String updateProfileApi = '/api/users/profile';
+//   static const String getUserByIdApi = '/api/users';
+//   static const String logoutApi = '/api/users/logout';
+//   static const String startInterviewSessionApi = '/interview-sessions';
+//   static const String startAiInterviewApi = '/interview-sessions/ai';
+//   static const String historyApi = "/users/me/interview-sessions";
+//   static String getSessionApi(int sessionId) =>
+//       '/interview-sessions/$sessionId';
+//
+//   static String answerMcqApi(int sessionId) =>
+//       '/interview-sessions/$sessionId/answer-mcq';
+//
+//   static String runCodeApi(int sessionId) =>
+//       '/interview-sessions/$sessionId/run-code';
+//
+//   static String submitCodeApi(int sessionId) =>
+//       '/interview-sessions/$sessionId/submit-code';
+//
+//   static String submitAnswerApi(int sessionId) =>
+//       '/interviews/$sessionId/submit';
+//
+// }
+
 class ApiEndpoint {
-  //todo static const String baseUrl = 'http://mockmate-001-site1.mtempurl.com';
   static const String baseUrl = '';
   static const String signupApi = '/api/users';
-  static const String loginApi = "/api/users/login";
+  static const String loginApi = '/api/users/login';
   static const String refreshTokenApi = '/api/users/refresh';
   static const String updateProfileApi = '/api/users/profile';
   static const String getUserByIdApi = '/api/users';
   static const String logoutApi = '/api/users/logout';
+
   static const String startInterviewSessionApi = '/interview-sessions';
   static const String startAiInterviewApi = '/interview-sessions/ai';
-  static const String historyApi = "/users/me/interview-sessions";
+  static const String historyApi = '/users/me/interview-sessions';
+
   static String getSessionApi(int sessionId) =>
       '/interview-sessions/$sessionId';
-
   static String answerMcqApi(int sessionId) =>
       '/interview-sessions/$sessionId/answer-mcq';
-
   static String runCodeApi(int sessionId) =>
       '/interview-sessions/$sessionId/run-code';
-
   static String submitCodeApi(int sessionId) =>
       '/interview-sessions/$sessionId/submit-code';
-
   static String submitAnswerApi(int sessionId) =>
       '/interviews/$sessionId/submit';
-    
 }
